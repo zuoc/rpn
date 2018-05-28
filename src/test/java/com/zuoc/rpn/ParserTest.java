7f498a4ed4b1286f7a018d96b9b6086d0c9e6cf0
杨金海 <jack_ctw@126.com>
2018-05-28 19:36:47 +0800

    参数化单元测试

--- src/test/java/com/zuoc/rpn/ParserTest.java
+++ src/test/java/com/zuoc/rpn/ParserTest.java
@@ -1,18 +1,39 @@
 package com.zuoc.rpn;
 
 import com.zuoc.rpn.exception.RpnParsingException;
-
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author zuoc
  */
+@RunWith(Parameterized.class)
 public class ParserTest {
+
+    private String expression;
+
+    public ParserTest(String expression) {
+        super();
+        this.expression = expression;
+    }
+
+    @Parameterized.Parameters
+    public static List<String> data() {
+        String[] data = {
+          "#{a_1} > 0.3 && #{b_2} <= -0.3",
+          "#{a_1} > 1.3 && #{b_2} <= -0.3"
+        };
+        return Arrays.asList(data);
+    }
 
     @Test
     public void testParse() throws RpnParsingException {
-        final Parser parser = Parser.compile("#{a_1} > 0.3 && #{b_2} <= -0.3", new ParserContext(new PlaceholderEval() {
+        final Parser parser = Parser.compile(expression, new ParserContext(new PlaceholderEval() {
             @Override
             public double eval(String placeholder) {
                 if (placeholder == null) {
