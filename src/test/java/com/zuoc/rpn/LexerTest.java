3620755148dcfabce3069cc20ce3261746511b2a
杨金海 <jack_ctw@126.com>
2018-05-28 19:36:47 +0800

    参数化单元测试

--- src/test/java/com/zuoc/rpn/LexerTest.java
+++ src/test/java/com/zuoc/rpn/LexerTest.java
@@ -1,25 +1,42 @@
 package com.zuoc.rpn;
 
-import com.zuoc.rpn.token.Other;
-import com.zuoc.rpn.token.Token;
-
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * @author zuoc
  */
+@RunWith(Parameterized.class)
 public class LexerTest {
+
+    private Object[] literals;
+    private Lexer lexer;
+
+    public LexerTest(Lexer lexer,Object[] literals) {
+        super();
+        this.literals = literals;
+        this.lexer = lexer;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] {
+          { new Lexer("#{a_1} > 1.3 && #{b_2} <= -0.3", "#{", "}"), new Object[]{"a_1",">","1.3","&&","b_2","<=","-0.3"} },
+          { new Lexer("#[a_1] > 1.3 && #[b_2] <= -0.3", "#[", "]"), new Object[]{"a_1",">","1.3","&&","b_2","<=","-0.3"} }
+        };
+        return Arrays.asList(data);
+    }
 
     @Test
     public void testNextToken() {
-        final Lexer lexer = new Lexer("#{a_1} > 1.3 && #{b_2} <= -0.3", "#{", "}");
-        Token currentToken;
-        while ((currentToken = lexer.nextToken()).getType() != Other.END) {
-            if (currentToken.getType() == Other.ERROR) {
-                System.out.println(currentToken);
-                break;
-            }
-            System.out.println(currentToken);
+        for (Object expected : literals) {
+            String actual = lexer.nextToken().getLiterals();
+            Assert.assertEquals(expected,actual);
         }
     }
 }
